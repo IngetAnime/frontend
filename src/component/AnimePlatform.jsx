7bb330a37dd740e9ff1d57c18b29d7204d4a0fad
Ahmad Subhan <asdcode123@gmail.com>
2025-06-11 06:54:37 +0700

    feat(anime): add more platforms

--- src/component/AnimePlatform.jsx
+++ src/component/AnimePlatform.jsx
@@ -5,9 +5,12 @@ export default function AnimePlatform({ platforms }) {
     <ul className="flex flex-nowrap overflow-x-auto gap-1">
       {platforms.map((platform, index) => {
         const icon = 
-          platform.platform.name.includes('AniOne') ? <img src='/images/anione.png' className="h-full w-full object-contain"/> :
-          platform.platform.name.includes('Muse') ? <img src='/images/muse.jpg' className="h-full w-full object-contain p-1"/> :
-          platform.platform.name.includes('Bstation') ? <img src='/images/bilibili.svg' className="h-full w-full object-contain"/>
+          platform.platform.name.includes('AniOne') ? <img src='/images/platforms/anione.png' className="h-full w-full object-contain"/> :
+          platform.platform.name.includes('Muse') ? <img src='/images/platforms/muse.jpg' className="h-full w-full object-contain p-1"/> :
+          platform.platform.name.includes('Bstation') ? <img src='/images/platforms/bilibili.svg' className="h-full w-full object-contain"/> :
+          platform.platform.name.includes('Catchplay') ? <img src='/images/platforms/catchplay.png' className="h-full w-full object-contain p-1"/> :
+          platform.platform.name.includes('Crunchyroll') ? <img src='/images/platforms/crunchyroll.svg' className="h-full w-full object-contain p-1"/> :
+          platform.platform.name.includes('Netflix') ? <img src='/images/platforms/netflix.png' className="h-full w-full object-contain px-0.75 py-1.25"/>
          : <></>;
         return (
           <li key={index} className="shrink-0 sm:pb-0">
