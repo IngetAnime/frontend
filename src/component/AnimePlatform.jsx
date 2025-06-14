5f5ce3a5decee7457d4954a3e57a8c541e8824e2
Ahmad Subhan <asdcode123@gmail.com>
2025-06-14 12:46:42 +0700

    feat(anime): add more platforms

--- src/component/AnimePlatform.jsx
+++ src/component/AnimePlatform.jsx
@@ -10,8 +10,9 @@ export default function AnimePlatform({ platforms }) {
           platform.platform.name.includes('Bstation') ? <img src='/images/platforms/bilibili.svg' className="h-full w-full object-contain"/> :
           platform.platform.name.includes('Catchplay') ? <img src='/images/platforms/catchplay.png' className="h-full w-full object-contain p-1"/> :
           platform.platform.name.includes('Crunchyroll') ? <img src='/images/platforms/crunchyroll.svg' className="h-full w-full object-contain p-1"/> :
-          platform.platform.name.includes('Netflix') ? <img src='/images/platforms/netflix.png' className="h-full w-full object-contain px-0.75 py-1.25"/>
-         : <></>;
+          platform.platform.name.includes('Netflix') ? <img src='/images/platforms/netflix.png' className="h-full w-full object-contain px-0.75 py-1.25"/> :
+          platform.platform.name.includes('Prime') ? <img src='/images/platforms/prime.png' className="h-full w-full object-contain px-0.75 py-1.5"/> : 
+          <></>;
         return (
           <li key={index} className="shrink-0 sm:pb-0">
             <ButtonLink sx={{ height: '2rem', minWidth: 'unset', p: 0 }} to={platform.link} target="blank">
