13ef41294a0717e21bca5695e186a58952f46044
Ahmad Subhan <asdcode123@gmail.com>
2025-07-09 09:28:37 +0700

    feat(anime): add footer

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -1,9 +1,42 @@
-import { Box, Avatar, Typography } from "@mui/material";
+import { Box, Avatar, Typography, Container, Toolbar, Divider, Menu, MenuItem, IconButton, List, ListItem } from "@mui/material";
+import Logo from "./Logo";
+import { GitHub, Instagram, LinkedIn } from "@mui/icons-material";
 
 export default function Footer() {
   return (
-    <Box sx={{ p: 2 }}>
-      <Typography component={'footer'} fontWeight={'medium'} textAlign={'center'}>2025 - IngetAnime</Typography>
+    <Box sx={{ bgcolor: 'primary.main', marginTop: 2 }}>
+      <Container className="py-4 text-white flex flex-col gap-5">
+        <Box className="flex items-center justify-between py-2 flex-col gap-5 md:gap-0 md:flex-row">
+          <Box className="flex items-center w-full justify-between md:justify-center gap-2 md:gap-0 md:max-w-fit">
+            <Logo color="white" />
+            <Typography sx={{ color: '#aaffe2' }} className="text-right md:text-left">
+              <span className="hidden md:inline">&nbsp;/</span> Cari tempat nonton anime terbaik
+            </Typography>
+          </Box>
+          <Typography>Designed and coded by Ahmad Subhan D</Typography>
+        </Box>
+        <Box className="flex items-center justify-between">
+          <Typography>&copy; 2025. All rights reserved.</Typography>
+          <List disablePadding className="flex">
+            <ListItem disablePadding>
+              <IconButton href="https://github.com/ahmadsubhand" target={'_blank'} sx={{ color: 'white' }}>
+                <GitHub />
+              </IconButton>
+            </ListItem>
+            <ListItem disablePadding>
+              <IconButton href="https://www.instagram.com/ahmadsubhand" target={'_blank'} sx={{ color: 'white' }}>
+                <Instagram />
+              </IconButton>
+            </ListItem>
+            <ListItem disablePadding>
+              <IconButton href="https://www.linkedin.com/in/ahmadsubhand/" target={'_blank'} sx={{ color: 'white' }}>
+                <LinkedIn />
+              </IconButton>
+            </ListItem>
+          </List>
+        </Box>
+        {/* <Typography component={'footer'} fontWeight={'medium'} textAlign={'center'}>2025 - IngetAnime</Typography> */}
+      </Container>
     </Box>
   )
 }
--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,12 +1,14 @@
 import { Outlet } from "react-router-dom"
 import Wrapper from "../component/Wrapper"
 import Header from "../component/Header"
+import Footer from "../component/Footer"
 
 export default function IndexPage() {
   return (
     <Wrapper>
       <Header />
       <Outlet />
+      <Footer />
     </Wrapper>
   )
 }
